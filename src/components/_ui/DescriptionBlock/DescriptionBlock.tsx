38638b4fc78b53351b37503fee5e9cb8a5125ad9
aidera <hope.aidera@gmail.com>
2024-05-23 18:15:19 -0400

    Updated about content

--- src/components/_ui/DescriptionBlock/DescriptionBlock.tsx
+++ src/components/_ui/DescriptionBlock/DescriptionBlock.tsx
@@ -1,13 +1,17 @@
+import classNames from 'classnames';
 import styles from './DescriptionBlock.module.scss';
 
 interface DescriptionBlockProps {
   title1: string;
+  title1Type?: 'h1' | 'h2' | 'h3' | 'h4';
   title2: string;
+  title2Type?: 'h1' | 'h2' | 'h3' | 'h4';
+  classNames?: string;
 }
 
 export default function DescriptionBlock(props: DescriptionBlockProps) {
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.container, props.classNames)}>
       <div className={styles.borders}>
         <span></span>
         <span></span>
@@ -16,8 +20,17 @@ export default function DescriptionBlock(props: DescriptionBlockProps) {
       </div>
 
       <div className={styles.content}>
-        <h1>{props.title1}</h1>
-        <h2>{props.title2}</h2>
+        {!props.title1Type && <h1>{props.title1}</h1>}
+        {props.title1Type === 'h1' && <h1>{props.title1}</h1>}
+        {props.title1Type === 'h2' && <h2>{props.title1}</h2>}
+        {props.title1Type === 'h3' && <h3>{props.title1}</h3>}
+        {props.title1Type === 'h4' && <h4>{props.title1}</h4>}
+
+        {!props.title2Type && <h2>{props.title2}</h2>}
+        {props.title2Type === 'h1' && <h1>{props.title2}</h1>}
+        {props.title2Type === 'h2' && <h2>{props.title2}</h2>}
+        {props.title2Type === 'h3' && <h3>{props.title2}</h3>}
+        {props.title2Type === 'h4' && <h4>{props.title2}</h4>}
       </div>
     </div>
   );
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,61 +1,144 @@
+import AnalyticsIcon from '../../assets/icons/AnalyticsIcon';
+import CaseIcon from '../../assets/icons/CaseIcon';
+import CategoryIcon from '../../assets/icons/CategoryIcon';
+import DesignIcon from '../../assets/icons/DesignIcon';
+import DevicesIcon from '../../assets/icons/DevicesIcon';
+import EducationIcon from '../../assets/icons/EducationIcon';
+import ForwardIcon from '../../assets/icons/ForwardIcon';
+import GroupIcon from '../../assets/icons/GroupIcon';
+import OfficeIcon from '../../assets/icons/OfficeIcon';
+import ToolsIcon from '../../assets/icons/ToolsIcon';
 import BackToMenuButton from '../../components/BackToMenuButton/BackToMenuButton';
 import PageWithViewer from '../../components/PageWithViewer/PageWithViewer';
 import PageWithViewerContent from '../../components/PageWithViewer/PageWithViewerContent/PageWithViewerContent';
 import PageWithViewerViewer from '../../components/PageWithViewer/PageWithViewerViewer/PageWithViewerViewer';
+import DescriptionBlock from '../../components/_ui/DescriptionBlock/DescriptionBlock';
+import styles from './AboutPage.module.scss';
 
 export default function AboutPage() {
   return (
     <PageWithViewer>
       <PageWithViewerContent title="About">
-        <p>
-          Frontend web developer with over 5 years of commercial experience.{' '}
-        </p>
-        <p>
-          - Leveraged cutting-edge frontend development tools, including React,
-          Angular, TypeScript, JavaScript (and more), to create dynamic,
-          adaptive and responsive interfaces.
-        </p>
-        <p>
-          - Developed the web applications for large and small companies in the
-          USA, Ukraine and Russia. Collaborated seamlessly with
-          cross-functional, international teams, including designers, project
-          managers, product managers, and backend developers.
-        </p>
-        <p>
-          - Delivered high-load corporate applications and landing pages in
-          various industries, such as: Internet communications and connection,
-          advertising, metal trading, recycling, insurance, events, restaurants
-          and food delivery.
-        </p>
-        <p>
-          - Specialized in adaptive designs for optimal user experiences across
-          desktop, tablet, and mobile platforms.
-        </p>
-        <p>
-          - Proficient in creating intuitive and visually appealing user
-          interfaces (UI) to optimize user experiences (UX) and drive engagement
-          with a proven ability to seamlessly translate both self-generated
-          designs and designs by other designers into functional code.
-        </p>
-        <p>
-          - Elevated user engagement through the integration of high-quality
-          animations and interactive features. Implemented SEO best practices to
-          enhance website and application visibility on leading search engines.
-          Successfully optimized the application to improve search engine
-          rankings and enhance overall performance.
-        </p>
-        <p>
-          - Conceptualized, designed, and executed a full-cycle web application,
-          overseeing all aspects from initial idea to deployment.
-        </p>
-        <p>
-          - Played a key role in mentoring and guiding junior developers,
-          fostering their growth and skill development.
-        </p>
-        <p>
-          - Demonstrated strong leadership skills in collaborative team
-          environments, ensuring effective communication and project execution.
-        </p>
+        <DescriptionBlock
+          title1="Eugenia Mikhaleva"
+          title1Type="h2"
+          title2="Frontend Developer"
+          title2Type="h3"
+          classNames={styles['description-block']}
+        ></DescriptionBlock>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <CaseIcon />
+          </div>
+          <p>
+            Frontend web developer with <strong>over 5 years</strong> of
+            commercial experience.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <ToolsIcon />
+          </div>
+          <p>
+            Leveraged cutting-edge frontend development tools, including{' '}
+            <strong>React, Angular, TypeScript, JavaScript</strong> (and more),
+            to create dynamic, adaptive and responsive interfaces.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <OfficeIcon />
+          </div>
+          <p>
+            Developed the web applications for{' '}
+            <strong>large and small companies</strong> in the USA, Ukraine and
+            Russia. Collaborated seamlessly with cross-functional, international
+            teams, including designers, project managers, product managers, and
+            backend developers.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <CategoryIcon />
+          </div>
+          <p>
+            Delivered high-load corporate applications and landing pages in{' '}
+            <strong>various industries</strong>, such as: AI, Internet
+            communications and connection, advertising, metal trading,
+            recycling, insurance, events, restaurants and food delivery.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <DevicesIcon />
+          </div>
+          <p>
+            Specialized in <strong>adaptive designs</strong> for optimal user
+            experiences across desktop, tablet, and mobile platforms.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <DesignIcon />
+          </div>
+          <p>
+            Proficient in creating intuitive and visually appealing user
+            interfaces to optimize user experiences <strong>(UI/UX)</strong> and
+            drive engagement with a proven ability to seamlessly translate both
+            self-generated designs and designs by other designers into
+            functional code.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <AnalyticsIcon />
+          </div>
+          <p>
+            Elevated user engagement through the integration of high-quality{' '}
+            <strong>animations and interactive features</strong>. Implemented
+            SEO best practices to enhance website and application visibility on
+            leading search engines. Successfully optimized the application to
+            improve search engine rankings and enhance overall performance.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <ForwardIcon />
+          </div>
+          <p>
+            Conceptualized, designed, and executed a full-cycle web application,
+            overseeing all aspects from initial idea to deployment.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <EducationIcon />
+          </div>
+          <p>
+            Played a key role in <strong>mentoring</strong> and guiding junior
+            developers, fostering their growth and skill development.
+          </p>
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.icon}>
+            <GroupIcon />
+          </div>
+          <p>
+            Demonstrated strong leadership skills in collaborative team
+            environments, ensuring effective communication and project
+            execution.
+          </p>
+        </div>
       </PageWithViewerContent>
       <PageWithViewerViewer menuItems={[<BackToMenuButton />]}>
         <img src={'/assets/images/bg1.png'} />
